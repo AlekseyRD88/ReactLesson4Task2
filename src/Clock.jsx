b531b410fcb4a6a9fb93e6650bf22f0e7ec3496a
= <neymegen50@gmail.com>
2022-07-29 10:09:50 +0300

    message5

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,33 +1,35 @@
 import React, { Component } from 'react';
+import moment from 'moment';
 import './clock.scss';
+
 
 const getTimeWithOffset = offset => {
   const currentTime = new Date();
   const utcOffset = currentTime.getTimezoneOffset() / 60;
   return new Date(currentTime.setHours(currentTime.getHours() + offset + utcOffset));
 }
-
+const formatDate = date => moment(date).format('h:mm:ss A');
 class Clock extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      location: this.props.location,
-      time: getTimeWithOffset(this.props.offset),
+      date: getTimeWithOffset(this.props.offset),
     };
     setInterval(() => {
       this.setState({
-        time: getTimeWithOffset(this.props.offset)
+        date: getTimeWithOffset(this.props.offset)
       });
     }, 1000);
   }
   render() {
+
     return(
       <div className="clock">
     <div className="clock__location">
-        {this.state.location}
+        {this.props.location}
     </div>
     <div className="clock__time">
-        {this.state.time}
+        {formatDate(this.state.date)}
     </div>
   </div>
     );
